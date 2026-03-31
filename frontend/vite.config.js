cf54d308449e3c19bf3b7bbf76ef9fce61016e04
RajendraKumar08 <rkdbprjr343040@gmail.com>
2026-04-01 01:53:08 +0530

    vite env error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const API = import.meta.env.VITE_BACKEND_URL
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const API = env.VITE_BACKEND_URL
 
-// https://vite.dev/config/
-export default defineConfig({
-  server : {
-    proxy : {
-      '/user/api' : API,
-      '/blog/api' : API,
-      '/comment/api' : API,
-      '/blog/api/search' : API,
-      '/chat/api' : API,
-      '/blog/api/:id/like' : API,
-      '/user/api/managelike' : API,
-      '/blog/api/:id/delete' : API,
-      '/blog/api/:id/view' : API,
-      '/user/api/:id' : API,
-      '/blog/api/:id/read-time' : API,
-    }
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
-
-
+  return defineConfig({
+    server: {
+      proxy: {
+        '/user/api': API,
+        '/blog/api': API,
+        '/comment/api': API,
+        '/blog/api/search': API,
+        '/chat/api': API,
+        '/blog/api/:id/like': API,
+        '/user/api/managelike': API,
+        '/blog/api/:id/delete': API,
+        '/blog/api/:id/view': API,
+        '/user/api/:id': API,
+        '/blog/api/:id/read-time': API,
+      }
+    },
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+  })
+}
