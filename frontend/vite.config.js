ae35bb3b7f07c014470abf4b619dfa9d7e6f3974
RajendraKumar08 <rkdbprjr343040@gmail.com>
2026-04-02 00:51:00 +0530

    fixed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const API = env.VITE_BACKEND_URL
-
-  return defineConfig({
-    // server: {
-    //   proxy: {
-    //     '/user/api': API,
-    //     '/blog/api': API,
-    //     '/comment/api': API,
-    //     '/blog/api/search': API,
-    //     '/chat/api': API,
-    //     '/blog/api/:id/like': API,
-    //     '/user/api/managelike': API,
-    //     '/blog/api/:id/delete': API,
-    //     '/blog/api/:id/view': API,
-    //     '/user/api/:id': API,
-    //     '/blog/api/:id/read-time': API,
-    //   }
-    // },
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-  })
-}
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
